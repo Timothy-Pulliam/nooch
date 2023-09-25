e249f8698fff8bfe84d7fac3cf93685c3d2e377e
Timothy Pulliam <timothy.dpulliam@gmail.com>
2023-09-25 11:53:52 -0400

    remove username field

--- models/userModel.js
+++ models/userModel.js
@@ -3,11 +3,6 @@ const bcrypt = require("bcrypt");
 
 const UserSchema = new mongoose.Schema(
     {
-        username: {
-            type: String,
-            required: true,
-            unique: true
-        },
         email: {
             type: String,
             required: true,
